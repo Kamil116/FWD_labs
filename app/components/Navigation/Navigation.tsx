fabf5abff56a98c504f0fba0e9f397491bfe9fea
Kamil116 <kamil.mirgasimoff@yandex.ru>
2024-07-06 19:26:07 +0300

    rewrote to react

--- app/components/Navigation/Navigation.tsx
+++ app/components/Navigation/Navigation.tsx
@@ -1,11 +1,16 @@
 import styles from '@/app/components/Navigation/navigation.module.css'
+import Link from "next/link";
 
 export default function Nav() {
     return (
         <>
             <div className={styles.topnav}>
-                <a href={'/about'}>About me</a>
-                <a href={'/jokes'}>Jokes</a>
+                <Link href={'/about'}>
+                    About me
+                </Link>
+                <Link href={'/jokes'}>
+                    Jokes
+                </Link>
             </div>
         </>
     )
